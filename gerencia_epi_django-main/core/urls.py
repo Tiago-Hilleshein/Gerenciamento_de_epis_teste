63523682fdd4ebc3da6a4b34b12e3e1e164ac99b
Tiago-Hilleshein <tiagohilleshein@gmail.com>
2025-10-28 17:36:17 -0300

    Add files via upload
    alteracoes feitas em base

---
+++ gerencia_epi_django-main/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('app_welcome', views.app_welcome, name='app_welcome'),
+    path('colaboradores/', views.colaboradores, name='colaboradores'),
+]
